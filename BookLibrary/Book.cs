6830b9a4fe06092d8d5777cacc3dddfe3f855e3d
MAnitsu <nitu.alexandru96@gmail.com>
2024-11-06 12:12:29 +0200

    Add project files.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,45 @@
+﻿namespace BookLibrary
+{
+    internal partial class Program
+    {
+        public class Book
+        {
+            private string Title;
+            private string Author;
+            private string Genre;
+            private string Publisher;
+            private int PageNr;
+
+            public Book(string title)
+            {
+                Title = title;
+            }
+            public Book(string title, string author)
+            {
+                Title = title;
+                Author = author;
+            }
+            public Book(string title, string author, string genre)
+            {
+                Title = title;
+                Author = author;
+                Genre = genre;
+            }
+            public Book(string title, string author, string genre, string publisher)
+            {
+                Title = title;
+                Author = author;
+                Genre = genre;
+                Publisher = publisher;
+            }
+            public Book(string title, string author, string genre, string publisher, int pageNr)
+            {
+                Title = title;
+                Author = author;
+                Genre = genre;
+                Publisher = publisher;
+                PageNr = pageNr;
+            }
+        }
+    }
+}
---
+++ BookLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your book library!\nTo see the command options type \"Help\".");
+            string input = Console.ReadLine();
+
+            if (input.ToLower().Equals("Help"))
+            {
+                Console.WriteLine("Commands available:" +
+                    "\nHelp - displays all the commands" +
+                    "\nAddBook - adds a book to the library\n" +
+                    "\nSeeLibary - displays all the books found in the library");
+            }
+        }
+    }
+}
